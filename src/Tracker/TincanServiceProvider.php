6d1ebc6c4df29c4ac317976c3790502f01eefd40
vagrant <vagrant@homestead>
2016-11-10 10:14:11 +0000

    changinf th route path

--- src/Tracker/TincanServiceProvider.php
+++ src/Tracker/TincanServiceProvider.php
@@ -14,7 +14,7 @@ class TincanServiceProvider extends ServiceProvider
    
     public function boot()
     {
-         require __DIR__.'/App/Http/routes.php';
+         require __DIR__.'/app/Http/routes.php';
         //$this->setupRoutes($this->app->router);
         // $this->loadViewsFrom(__DIR__ .'/../views','todo');
     }
