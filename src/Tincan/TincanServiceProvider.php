8f1e662b5551db96c073c216a270ca8f58c42e48
vagrant <vagrant@homestead>
2016-11-11 06:40:34 +0000

    adding controller path

--- src/Tincan/TincanServiceProvider.php
+++ src/Tincan/TincanServiceProvider.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace Lrs\Tracker;
+namespace Lrs\Tincan;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\Router;
 
 class TincanServiceProvider extends ServiceProvider
 {
@@ -12,29 +11,13 @@ class TincanServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    
    
     public function boot()
     {
          require __DIR__.'/app/Http/routes.php';
-        // $this->setupRoutes($this->app->router);
+        //$this->setupRoutes($this->app->router);
         // $this->loadViewsFrom(__DIR__ .'/../views','todo');
     }
-
-    /**
-     * Define the routes for the application.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
-    // public function setupRoutes(Router $router)
-    // {
-    //     $router->group(['namespace' => 'Lrs\Tincan\app\Http\Controllers'], function($router)
-    //     {
-    //         require __DIR__.'/app/Http/routes.php';
-    //     });
-    // }
-
 
      /**
      * Register any application services.
